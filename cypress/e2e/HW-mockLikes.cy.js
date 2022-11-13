f716ea623ce9d609b9256358d172c5c9fb332126
Alina Kypryk <kypryk.a@gmail.com>
2022-11-13 22:37:42 +0200

    corrected HW

--- cypress/e2e/HW-mockLikes.cy.js
+++ cypress/e2e/HW-mockLikes.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
-import AuthorizationPage from '../support/pages/AuthorizationPage'
-import user from '../fixtures/user.json'
+
 import {login, loginViaAPI} from '../support/helper'
 import HomePage from '../support/pages/HomePage'
 import myArticle from '../fixtures/myArticle.json'
@@ -14,17 +13,23 @@ beforeEach('Login', () => {
 describe('Positive cases', () => {
 
   it('Get article and mock addition and removal of likes', {retries: 2}, () => {
+    
+    
+    cy.intercept ('GET', '/api/articles?limit=10&offset=0', myArticle);
     HomePage.visit()
-    cy.intercept ('POST', '/api/articles', myArticle);
     HomePage.getGlobalFeed().click();
     
-
-    myArticle.articles[0].favoritesCount = '15';
+    myArticle.articles[0].favoritesCount = 15;
     let favoriteBody = {"article": myArticle.articles[0]}
-    console.log(favoriteBody)
+    //console.log(favoriteBody)
     let articleName = myArticle.articles[0].slug
-    cy.intercept ('POST', `**/${articleName}/favorite`, favoriteBody)
+    
     cy.get('.article-preview a[href="/profile/eileen"]').siblings('.pull-xs-right').click(); 
+    cy.intercept ('POST', `**/${articleName}/favorite`, favoriteBody)
+    cy.get('.article-preview a[href="/profile/eileen"]').siblings('.pull-xs-right').should('contain', '16')
+
+    cy.get('.article-preview a[href="/profile/eileen"]').siblings('.pull-xs-right').click(); 
+    cy.get('.article-preview a[href="/profile/eileen"]').siblings('.pull-xs-right').should('contain', '15')
     
   })
 
